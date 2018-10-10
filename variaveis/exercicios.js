852013318c3905fb0b6ff73b3e3512d49a28dc36
Bruno Alb <brunoalbuquerque.258@gmail.com>
2018-10-10 02:58:40 -0300

    first commit
    variavis e tipos

---
+++ variaveis/exercicios.js
@@ -0,0 +1,26 @@
+//Declarar uma variavel com seu nome
+
+var nome = 'bruno';
+console.log(nome)
+
+//Declarar uma variavel com sua idade 
+
+var idade ='21';
+console.log(idade)
+
+// Declarar uma variavel com sua comida favoria e nao atribuir valor 
+
+var comida 
+console.log(comida)
+
+comida = 'pizza';
+
+console.log(comida)
+
+
+// Declarar 5 variáveis diferentes sem valores
+var variavel1,
+  variavel2,
+  variavel3,
+  variavel4,
+  variavel5;
